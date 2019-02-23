34da25593493ffe07399457c9d685e1143bafd7d
chelo26 <m.gutierrez@braintree.com>
2019-02-23 14:33:24 +0000

    commit_line

--- MessageHandlerBot.py
+++ MessageHandlerBot.py
@@ -49,7 +49,7 @@ class MessageHandlerBot():
         return post
 
     def store_message(self,db,post):
-        #message_to_post = self.generate_post()
+
         db.insert(post)
 
     def find_last_message_received(self,db,sender_id):
@@ -60,12 +60,26 @@ class MessageHandlerBot():
         #print(parsed_message)
         return parsed_message["message"]
 
-    def find_last_message_sent(self,db):
-        message_entry = db.find({"recipient_id":{"$exists": False}}).sort("timestamp",-1).limit(1)
+    def find_last_message_sent(self,db,sender_id):
+        #message_entry = db.find({"recipient_id":{"$exists": False}},{"sender_id":sender_id}}).sort("timestamp",-1).limit(1)
 
-        #message_entry = db.find({"sender_id":{"$ne" : sender_id}}).sort("timestamp",-1).limit(1)
+        message_entry = db.find({"sender_id":{"$ne" : sender_id}}).sort("timestamp",-1).limit(1)
         #print("last message sent: ")
         #print(message_entry)
+        try:
+            parsed_message = next(message_entry)
+            #print(parsed_message)
+        except StopIteration:
+            parsed_message = {"message":None}
+            #print(parsed_message)
+            pass
+        return parsed_message["message"]
+
+    def get_messages_sequence(self,db,sender_id):
+        message_entry = db.find({"sender_id":sender_id}).sort("timestamp",-1)
+
+        print("sequence: ")
+        print(list(message_entry))
         try:
             parsed_message = next(message_entry)
             #print(parsed_message)
@@ -78,32 +92,7 @@ class MessageHandlerBot():
 
 
 
-    #def echo_talk():
-    #    bot.send(self.last_sender_id, self.last_message)
-                    # Response:
-                    # response = self.last_message
-                    # bot.send_text_message(self.sender_id, response)
 
-                    # if messaging_event.get('message'):
-                    #     self.parse_message(messaging_event)
-                    #
-    				# 	# Extracting text message
-                    #     if 'text' in messaging_event['message']:
-                    #         messaging_text = messaging_event['message']['text']
-                    #     else:
-                    #         messaging_text = 'no text'
-                    #
-    				# 	# Echo
-    				# 	# response = messaging_text
-    				# 	# bot.send_text_message(sender_id, response)
-                    #     response = None
-                    #
-                    #     entity, value = wit_response(messaging_text)
-                    #     if entity == "location":
-                    #         response = "Intersting, so what city in {} you come from? ".format(str(value))
-                    #     if response == None:
-                    #         respones = "Sorry I don't understand"
 
-    # def get_entity_value():
-    #     self.entity
-    # def start_conversation():
+
+
--- app.py
+++ app.py
@@ -1,71 +1,193 @@
-import os
-import sys
+# -*- coding: utf-8 -*-
+import os, sys
+import pprint
 from flask import Flask, request
-from utils import wit_response
+from MessageHandlerBot import MessageHandlerBot
+from pymongo import MongoClient
+import creds as CR
 from pymessenger import Bot
+import re
+from datetime import datetime
 
+# TIPICAL GREETINGS:
+GREETINGS = set(["salut!","salut","hello","hi","holaa","hola","hey","holas","heyy","hii","hiii"])
+
+questions = {'0':'Are you looking for a place to stay tonight?',
+             '1':'Where are you ?',
+             '2':'We\'re looking for the service that\'s best for you.\nHow old are you?'}
+
+answers = {'0':['yes','no']}
+
+
+
+
+
+
+
+# Initialize database:
+def point_collection():
+    client = MongoClient(CR.mongo_host,CR.mongo_port)
+    db = client[CR.mongo_db]
+    collection = db.messages
+    return collection
+
+def clean_message(message):
+    message = message.lower()
+    message = re.sub(r'[^\w]', '', message)
+    return message
+
+def generate_post(sender_id,message):
+    post = {"sender_id": sender_id,
+            #"recipient_id": self.last_recipient_id,
+            "message": message,
+            "timestamp": datetime.utcnow()}
+    return post
+
+def encode_received_message(message):
+    return
+
+
+# Get a reply:
+def get_reply(last_client_message,last):
+    last_message = clean_message(last_client_message)
+
+
+
+
+
+# Find an answer:
+
+def answer_to_message(last_message_received,last_message_sent):
+    # Cleaning message:
+    if last_message_sent:
+        last_message_sent = clean_message(last_message_sent)
+    if last_message_received:
+        last_message_received = clean_message(last_message_received)
+
+    # Different messages:
+    saludo = "Hi welcome to De Paul! \nHow could we help you ?"
+    buttons = [
+            {
+            "type":"web_url",
+            "url":"http://www.paginasiete.bo/",
+            "title":"Bolivian News"
+            },
+            {
+              "type":"web_url",
+              "url":"http://www.paginasiete.bo/",
+              "title":"Bolivian News"
+              },
+
+              {
+                "type": "postback",
+                "title": "otro",
+                "payload": "balbal"
+               }]
+
+    if last_message_received in GREETINGS:
+        return (saludo,"text")
+
+    elif "yes" in last_message_received: #and last_message_sent == saludo:
+        offers = "great, I can offer you:"
+        return (offers,buttons,"options")
+
+    elif last_message_received =="otro":
+        return ("what ? ","text")
+
+    else:
+        return ("To complete","text")
+
+
+
+
+# Messages database:
+messages_table = point_collection()
+print('database')
+# Bots:
+#listener_bot = MessageHandlerBot()
+responder_bot = Bot(CR.PAGE_ACCESS_TOKEN)
+
+
+# App:
 app = Flask(__name__)
-
-PAGE_ACCESS_TOKEN = "EAAbbVAxRX3oBAJyGezTZBJOECyyIMxJU50A5gODBZBtMDLiXZCxLZAofWOsiQtBOjBNH0YwyZAYzK3ZCQ8tgDJJTSWjtZBOZA4zUFURwREqcYeIEyfvxWMxDi9DKL011gmn1svsngNxZBDHvoWpZAheZCksdQEElfqnbz62JO5XwjaZC4gZDZD"
-
-bot = Bot(PAGE_ACCESS_TOKEN)
-
 
 @app.route('/', methods=['GET'])
 def verify():
 	# Webhook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-
         if not request.args.get("hub.verify_token") == "hello":
-
             return "Verification token mismatch", 403
-
         return request.args["hub.challenge"], 200
+    #print(request.args)
     return "Hello world", 200
-
 
 @app.route('/', methods=['POST'])
 def webhook():
+    # Get the message in Json format:
 
-    data = request.get_json()
-    log(data)
-    if data['object'] == 'page':
+    message_event = request.get_json()
+    print("message event received: ")
+    pprint.pprint(message_event)
+    print("")
+    print("")
+    # Parse message:
 
-        for entry in data['entry']:
-            for messaging_event in entry['messaging']:
-                # IDs
-                sender_id = messaging_event['sender']['id']
-                recipient_id = messaging_event['recipient']['id']
+    # 1. Bot parses the message
+    listener_bot = MessageHandlerBot()
 
-                if messaging_event.get('message'):
+    # If it receives a text message or a payload message:
+    last_client_message, send_back_bool = listener_bot.parse_json_message(message_event)
+    last_client_message = clean_message(last_client_message)
+    print("last client message: ", last_client_message)
 
-					# Extracting text message
-                    if 'text' in messaging_event['message']:
-                        messaging_text = messaging_event['message']['text']
-                    else:
-                        messaging_text = 'no text'
+    #print("send back bool : ", send_back_bool)
+    #print("")
 
-					# Echo
-					# response = messaging_text
-					# bot.send_text_message(sender_id, response)
-                    response = None
+    # If we should message back:
+    if send_back_bool == True:
 
-                    entity, value = wit_response(messaging_text)
-                    if entity == "location":
-                        response = "Intersting, so what city in {} you come from? ".format(str(value))
-                    if response == None:
-                        respones = "Sorry I don't understand"
+        # 2. Bot stores the message
+        post = listener_bot.generate_post()
+        listener_bot.store_message(messages_table,post)
 
-                    bot.send_text_message(sender_id,response)
+        # 3.Finding the sender_id:
+        sender_id = listener_bot.sender_id
 
+        # 3.2 Finding last messages:
+        #last_message_received = listener_bot.find_last_message_received(messages_table,sender_id)
+
+        #qa_sequence = listener_bot.find_last_message_sent(messages_table,sender_id)
+        qa_sequence = listener_bot.get_messages_sequence(messages_table, sender_id)
+
+        last_server_question = list(qa_sequence)
+        print("last question sent by server: ", last_server_question)
+
+        # 4. Sending the answer:
+        #answer_tuple = answer_to_message(last_message_received,last_message_sent)
+        server_next_question = 'what?'
+
+        #server_reply = get_reply(last_client_message,last_server_question)
+
+        post = generate_post(sender_id,server_next_question)
+        listener_bot.store_message(messages_table,post)
+
+        responder_bot.send_text_message(sender_id, server_next_question)
+        # if len(answer_tuple) == 2:
+        #     answer = answer_tuple[0]
+        #     print("")
+        #     print("answer to send: ", server_next_question)
+        #     print("")
+        #     responder_bot.send_text_message(sender_id, server_next_question)
+        #     #responder_bot.send_text_message(sender_id, "test")
+        # else:
+        #     offers = answer_tuple[0]
+        #     buttons = answer_tuple[1]
+        #     responder_bot.send_button_message(sender_id,offers,buttons)
+    else:
+        pass
 
     return "ok", 200
 
 
-def log(message):
-	print(message)
-	sys.stdout.flush()
-
-
 if __name__ == "__main__":
-	app.run(debug = True, port = 80)
+	app.run(host = "0.0.0.0",port=5000)
